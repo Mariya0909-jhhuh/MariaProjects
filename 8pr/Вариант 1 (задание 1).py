f7ab0fad8bc2f70b605902cce0b6c5ac0ddb4162
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-25 19:49:08 +0300

    Update Вариант 1 (задание 1).py

--- 8pr/Вариант 1 (задание 1).py
+++ 8pr/Вариант 1 (задание 1).py
@@ -1,1 +1,21 @@
-
+m=int(input())
+a=[]
+summa=0
+count=0
+for i in range(m):
+    b=[]
+    for j in range(m):
+        b.append(int(input()))
+    a.append(b)
+for i in a:
+    print()
+    for j in i:
+        print(j, end='  ')
+for i in range(m):
+    for j in range(i+1, m):
+        if a[i][j]>0:
+            count+=1
+            summa+=a[i][j]
+print()
+print(summa)
+print(count)
