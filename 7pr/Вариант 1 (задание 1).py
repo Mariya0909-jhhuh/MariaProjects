70a6397b8ab86ed183cb69839fdd634e5cde364e
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-25 19:44:39 +0300

    /

--- 7pr/Вариант 1 (задание 1).py
+++ 7pr/Вариант 1 (задание 1).py
@@ -1,1 +1,17 @@
-
+from math import *
+figure=input("Введите название фигуры с маленькой буквы: ")
+def s():
+    if figure=='круг':
+        r=float(input('Радиус круга: '))
+        area=pi*(r**2)
+        print(area)
+    elif figure=='квадрат':
+        a=int(input('Введите сторону квадрата: '))
+        area=a**2
+        print(area)
+    elif figure=='треугольник':
+        a=int(input('Введите основание треугольника: '))
+        h=int(input('Введите высоту треугольника: '))
+        area=(1/2)*a*h
+        print(area)
+s()
