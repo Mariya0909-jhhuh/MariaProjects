afe18672724b9e997868d247e449cb53f6402084
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-25 19:50:18 +0300

    Update Блок А (задание 1).py

--- 9pr/Блок А (задание 1).py
+++ 9pr/Блок А (задание 1).py
@@ -1,1 +1,7 @@
-
+x=int(input('Введите натуральное число:'))
+n=int(input('Введите число факториал которого хотите найти:'))
+def factrorial(n):
+    if n==0:
+        return 1
+    return factrorial(n-1)*n
+print((x**n)/factrorial(n))
