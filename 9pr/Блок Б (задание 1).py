4ec81331c85820308ff2822fd7edc74b58b95aff
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-25 19:51:05 +0300

    Update Блок Б (задание 1).py

--- 9pr/Блок Б (задание 1).py
+++ 9pr/Блок Б (задание 1).py
@@ -1,1 +1,9 @@
-
+def maxi_ts(n, maxi):
+    ts = n % 10
+    if ts > maxi:
+        maxi = ts
+    if n > 9:
+        return maxi_ts(n // 10, maxi)
+    return maxi
+n= int(input())
+print(maxi_ts(n, -1))
