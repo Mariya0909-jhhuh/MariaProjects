955ebc65458cf304f7761db8b55b2f0ecf96ce17
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-25 19:35:33 +0300

    /

--- 6pr/Вариант 2.py
+++ 6pr/Вариант 2.py
@@ -1,1 +1,17 @@
-
+#1
+n=int(input())
+n=[int(input()) for i in range(n)]
+print(min(n))
+print(n.index(min(n)))
+#2
+n=int(input())
+n=[int(input()) for i in range(n)]
+p=[]
+o=[]
+for j in range(len(n)):
+    if n[j]>0:
+        p.append(n[j])
+    else:
+        o.append(n[j])
+print(p)
+print(o)
