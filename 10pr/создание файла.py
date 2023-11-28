4b93558a692c73499086c2af7cb50eca69710c1e
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-11-28 19:41:36 +0300

    /

--- 10pr/создание файла.py
+++ 10pr/создание файла.py
@@ -1,1 +1,16 @@
-
+from random import randint
+n = int(input())
+m = int(input())
+a = []
+for i in range(n):
+    b=[]
+    for j in range(m):
+        b.append(randint(1,50))
+    a.append(b)
+f = open(r'Mariya_Slepicheva_UB-31_vvod.txt', 'w')
+for i in a:
+    for j in i:
+        f.write(str(j))
+        f.write(' ')
+    f.write("\n")
+f.close()
