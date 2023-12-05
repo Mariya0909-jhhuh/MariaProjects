1ef64a562bf3c0e55de3eb0e7d0c46113e84691a
Mariya0909-jhhuh <145536119+Mariya0909-jhhuh@users.noreply.github.com>
2023-12-05 10:20:50 +0300

    Update json.py

--- 12pr/json.py
+++ 12pr/json.py
@@ -1,1 +1,24 @@
-
+import requests
+import json
+from tkinter import *
+def information_button():
+    name = text.get()
+    link = f"https://api.github.com/users/{name}"
+    response = requests.get(link).json()
+    keys = ['company','created_at','email','id','name','url']
+    response_new = {}
+    for i in keys:
+        response_new[i] = response[i]
+    with open('information.txt','w') as file:
+        json.dump(response_new,file,indent=3)
+window = Tk()
+window.title('Репозитории')
+window.configure(background='grey')
+window.geometry('300x300')
+lbl = Label(window, text='Введите название репозитория', width=30, height=1, bg='grey', font=45)
+lbl.grid(row=0, column=1, padx=10)
+text = Entry(window, width=20)
+text.grid(column=1, row=1, pady=20)
+button = Button(window ,text='Получить информацию',width=30,height=1, command=information_button)
+button.grid(row=3, column=1, pady=20)
+window.mainloop()
